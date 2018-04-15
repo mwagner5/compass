ca51a2a4af025086beaa904bc01964df9aebd20e
mwagner5 <38388201+mwagner5@users.noreply.github.com>
2018-04-15 13:50:47 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,49 +4,48 @@ var east_txt = "east";
 var south_text = "south";
 var west_text = "west";
 var north_text = "north";
-var gif;
+var gif_south;
 
 function setup() {
 	createCanvas(window.innerWidth, window.innerHeight, P2D);
-	gif = loadGif('waves.gif');
+	gif_south = loadGif('waves.gif');
 }
 
 function draw() {
-  // clears the canvas each time draw runs
-  background(255);
-
-//set some properties
-  fill(50);
-  textSize(100);
-  textAlign(CENTER);
-  // translate moves the grid so that the (0,0) point is at the center of the canvas rather than in the top left
-  translate(width/2, height/2);
-  //text("direction", 0, 0);
-
-  if(compassHeading > 45 && compassHeading < 135) {
-	  text(east_txt,0,0);
-  } else if (compassHeading >= 135 && compassHeading < 225) {
-	  //text(south_text,0,0);
-	  image(gif, 0, 0);
-  } else if (compassHeading >= 225 && compassHeading < 315) {
-	  text(west_text,0,0);
-  } else if (compassHeading >= 315 && compassHeading < 359 || compassHeading >= 0 && compassHeading <=45) {
-	  text(north_text,0,0);
-  } else {
-	  text("Rose",0,0);
-  }
-
+	
+	// clears the canvas each time draw runs
+	background(255);
+	
+	imageMode(CENTER);
+	//set some properties
+	fill(50);
+	textSize(100);
+	textAlign(CENTER);
+	// translate moves the grid so that the (0,0) point is at the center of the canvas rather than in the top left
+	translate(width/2, height/2);
+	
+	if(compassHeading > 45 && compassHeading < 135) {
+		text(east_txt,0,0);
+	} else if (compassHeading >= 135 && compassHeading < 225) {
+		//text(south_text,0,0);
+		image(gif_south, 0, 0);
+	} else if (compassHeading >= 225 && compassHeading < 315) {
+		text(west_text,0,0);
+	} else if (compassHeading >= 315 && compassHeading < 359 || compassHeading >= 0 && compassHeading <=45) {
+		text(north_text,0,0);
+	} else {
+		text("Rose",0,0);
+	}
 }
 
 // javascript for device orientation 
-window.addEventListener('deviceorientation', function(e) 
-{
-  alpha = e.alpha;
-  beta = e.beta;
-  gamma = e.gamma;
-  if(e.webkitCompassHeading) {
-            compassHeading = e.webkitCompassHeading;
-        }   else  { 
-            compassHeading = alpha;
-        }
+window.addEventListener('deviceorientation', function(e) {
+	alpha = e.alpha;
+	beta = e.beta;
+	gamma = e.gamma;
+	if(e.webkitCompassHeading) {
+		compassHeading = e.webkitCompassHeading;
+	} else {
+		compassHeading = alpha;
+	}
 });
