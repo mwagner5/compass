1c8c3e8c94143ea1f3104f19ae2f7c6e285c9c40
mwagner5 <38388201+mwagner5@users.noreply.github.com>
2018-04-14 20:53:24 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,46 @@
+var compassHeading = 0;
 var capture;
+var east_txt = "east"
 
 function setup() {
-  createCanvas(480, 120);
-  capture = createCapture(VIDEO);
+	createCanvas(window.innerWidth, window.innerHeight, P2D);
+	capture = createCapture(VIDEO);
 }
 
 function draw() {
+  // clears the canvas each time draw runs
+  background(255);
+
+//set some properties
+  //rotateX(radians(rotationX));
+  //rotateY(radians(rotationY));
+  fill(50);
+  textSize(100)
+  textAlign(CENTER)
+  // translate moves the grid so that the (0,0) point is at the center of the canvas rather than in the top left
+  translate(width/2, height/2)
+  
+
+  if(compassHeading > 45 && compassHeading < 135) {
+  		text(east_txt,0,0)
+  } else {
+  		text(compassHeading,0,0)
+  }
+
   image(capture, 0, 0, width, width * capture.height / capture.width);
-  filter(INVERT);
+  //filter(INVERT);
+
 }
+
+// javascript for device orientation 
+window.addEventListener('deviceorientation', function(e) 
+{
+  alpha = e.alpha;
+  beta = e.beta;
+  gamma = e.gamma;
+  if(e.webkitCompassHeading) {
+            compassHeading = e.webkitCompassHeading;
+        }   else  { 
+            compassHeading = alpha;
+        }
+});
