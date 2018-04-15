31e6fe780e3453e729ba5e31c8fec54cccc609f6
mwagner5 <38388201+mwagner5@users.noreply.github.com>
2018-04-15 10:10:07 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 var compassHeading = 0;
 var capture;
-var east_txt = "east"
-var south_text = "south"
+var east_txt = "east";
+var south_text = "south";
+var west_text = "west";
+var north_text = "north";
 
 function setup() {
 	createCanvas(window.innerWidth, window.innerHeight, P2D);
@@ -15,26 +17,26 @@ function draw() {
   //rotateX(radians(rotationX));
   //rotateY(radians(rotationY));
   fill(50);
-  textSize(100)
-  textAlign(CENTER)
+  textSize(100);
+  textAlign(CENTER);
   // translate moves the grid so that the (0,0) point is at the center of the canvas rather than in the top left
-  translate(width/2, height/2)
-  
+  translate(width/2, height/2);
+  text("direction", 0, 0);
 
   if(compassHeading > 45 && compassHeading < 135) {
-  		text(east_txt,0,0);
+	  text(east_txt,0,0);
   } else if (compassHeading >= 135 && compassHeading < 225) {
-  		text(south_text,0,0);
-
+	  text(south_text,0,0);
+  } else if (compassHeading >= 225 && compassHeading < 315) {
+	  text(west_text,0,0);
+  } else if (compassHeading >= 315 && compassHeading < 359 || compassHeading >= 0 && compassHeading <=45) {
+	  text(north_text,0,0);
   } else {
-  		rotateX(radians(rotationX));
-  		rotateY(radians(rotationY));
-  		text("I am an android!",0,0);
-
+	  text("Rose",0,0);
   }
-
-  image(capture, 0, 0, width, width * capture.height / capture.width);
-  //filter(INVERT);
+// 	rotateX(radians(rotationX));
+// 	rotateY(radians(rotationY));
+// 	text("I am an android!",0,0);
 
 }
 
